4f389e19dd8852b63bb3adf2edd36035ea96fab6
Sandra Coburn <sandra_coburn@hotmail.com>
2020-10-05 19:22:33 -0700

    Added an error page to handle routing errors

---
+++ pages/_error.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+import Router from "next/router";
+
+const errorPage = () => (
+  <div>
+    <h1>Oops, something went wrong.</h1>
+    <p>
+      Try
+      <Link href="/">
+        <a>going back</a>
+      </Link>
+    </p>
+    <button onClick={() => Router.push("/auth")}>Go to Auth</button>
+  </div>
+);
+
+export default errorPage;
